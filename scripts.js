bcd173aa81f9c0f35b8da390e7f2af697cefa7dd
MJ <mickeymouse9886@gmail.com>
2025-09-17 14:38:48 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const toggleBtn = document.getElementById('toggleVideoBtn');
 const seizureVideoLeft = document.getElementById('seizureVideoLeft');
 const seizureVideoRight = document.getElementById('seizureVideoRight');
 const avatar = document.querySelector('.avatar');
+const heading = document.querySelector('h1');
+const subtext = document.querySelector('p');
 
 let isPlaying = false;
 
@@ -17,6 +19,8 @@ toggleBtn.addEventListener('click', async () => {
         seizureVideoRight.currentTime = 0;
         await Promise.all([seizureVideoLeft.play(), seizureVideoRight.play()]);
         toggleBtn.textContent = 'MAKE IT STOP';
+        heading.textContent = 'Good morning';
+        subtext.textContent = 'Long time no see!';
         if (isMobile()) {
             avatar.classList.add('hide-avatar-mobile');
         }
@@ -26,6 +30,8 @@ toggleBtn.addEventListener('click', async () => {
         seizureVideoLeft.style.display = 'none';
         seizureVideoRight.style.display = 'none';
         toggleBtn.textContent = 'SEIZURE ALERT';
+        heading.textContent = 'Ohayo!';
+        subtext.textContent = 'Hisashiburi dana';
         avatar.classList.remove('hide-avatar-mobile');
     }
     isPlaying = !isPlaying;
